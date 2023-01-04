ed339f37b608f069717bd0574a3d6f99c1caca70
0carloscastillo0 <carlos.castillo.d@usach.cl>
2023-01-04 13:53:06 -0300

    api-server

---
+++ API-SERVER/backend/src/main/java/com/example/backend/controller/TweetController.java
@@ -0,0 +1,125 @@
+package com.example.backend.controller;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.backend.model.Tweet;
+import com.example.backend.service.TweetService;
+
+@RestController
+@RequestMapping("tweets")
+public class TweetController {
+    private final TweetService tweetService;
+
+    public TweetController(TweetService tweetService) {
+        this.tweetService = tweetService;
+    }
+
+    //--------------------------------------------------------------------------------------------
+    //                              CONSULTAS IMPLEMENTADAS
+    //--------------------------------------------------------------------------------------------
+    // Mostrar la informacion de todos los tweets extraidos de la base de datos
+    @GetMapping(value="/all")
+    public List<Tweet> getAll() throws InterruptedException, ExecutionException{
+        // Se llama al servicio
+        CompletableFuture<List<Tweet>> t = tweetService.getAll();
+        
+        //Se espera a que termine el servicio
+        CompletableFuture.allOf(t).join();
+
+        //Se extrae el resultado del proceso de la ejecucion
+        List<Tweet> list = Stream.of(t.get())
+            .flatMap(Collection::stream)
+            .collect(Collectors.toList());
+
+
+        return list;
+    }
+    //--------------------------------------------------------------------------------------------
+    // Mostrar la cantidad de tweets cargados
+    @GetMapping(value="/countTweets")
+    public long count() throws InterruptedException, ExecutionException{
+        // Se llama al servicio
+        CompletableFuture<Long> t = tweetService.count();
+
+        // Se espera a que termine
+        CompletableFuture.allOf(t).join();
+
+        //Se extrae el resultado del proceso
+        long count = t.get();
+
+        return count;
+    }
+
+    //------------------------------------------------------------------------------------------
+    // Mostrar la media de la cantidad de favoritos
+    @GetMapping(value="/meanFav")
+    public double meanFav() throws InterruptedException, ExecutionException{
+        // Se llama a los servicios
+        CompletableFuture<Long> t1 = tweetService.count();
+        CompletableFuture<Double> t2 = tweetService.sumFav();
+
+        // Se espera a que terminen los servicios
+        CompletableFuture.allOf(t1, t2).join();
+
+        // Se extraen los servicios y se realiza el calculo
+        double mean = t2.get()/t1.get();
+        return mean;
+    }
+
+    //------------------------------------------------------------------------------------------
+    // Mostrar la varianza de la cantidad de favoritos
+    @GetMapping(value="/varFav")
+    public double varFav() throws InterruptedException, ExecutionException{
+        // Se llama a los servicios
+        CompletableFuture<Double> t1 = tweetService.stdFav();
+        CompletableFuture<Double> t2 = tweetService.stdFav();
+
+        // Se espera a que terminen los servicios
+        CompletableFuture.allOf(t1, t2).join();
+
+        // Se extraen los servicios y se realiza el calculo
+        double var = t2.get() * t1.get();
+        return var;
+    }
+
+    //------------------------------------------------------------------------------------------
+    // Mostrar la media de la cantidad de retweets
+    @GetMapping(value="/meanRet")
+    public double meanRet() throws InterruptedException, ExecutionException{
+        // Se llama a los servicios
+        CompletableFuture<Long> t1 = tweetService.count();
+        CompletableFuture<Double> t2 = tweetService.sumRet();
+
+        // Se espera a que terminen los servicios
+        CompletableFuture.allOf(t1, t2).join();
+
+        // Se extraen los servicios y se realiza el calculo
+        double mean = t2.get()/t1.get();
+        return mean;
+    }
+
+    //------------------------------------------------------------------------------------------
+    // Mostrar la varianza de la cantidad de retweets
+    @GetMapping(value="/varRet")
+    public double varRet() throws InterruptedException, ExecutionException{
+        // Se llama a los servicios
+        CompletableFuture<Double> t1 = tweetService.stdRet();
+        CompletableFuture<Double> t2 = tweetService.stdRet();
+
+        // Se espera a que terminen los servicios
+        CompletableFuture.allOf(t1, t2).join();
+
+        // Se extraen los servicios y se realiza el calculo
+        double var = t2.get() * t1.get();
+        return var;
+    }
+}
---
+++ API-SERVER/frontend/vue-argon-dashboard-main/src/views/components/AuthorsTable.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="card">
+    <div class="card-header pb-0">
+      <h6>Authors table</h6>
+    </div>
+    <div class="card-body px-0 pt-0 pb-2">
+      <div class="table-responsive p-0">
+        <table class="table align-items-center mb-0">
+          <thead>
+            <tr>
+              <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Tweets totales</th>
+              <th
+                class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2"
+              >Media Retweets</th>
+              <th
+                class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"
+              >Varianza Retweets</th>
+              <th
+                class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"
+              >Media Favoritos</th>
+              <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"
+              >Varianza Favoritos
+              </th>
+            </tr>
+          </thead>
+          <tbody >
+            <tr>
+              <td>
+                <div class="d-flex px-2 py-1">
+                  <div class="d-flex flex-column justify-content-center">
+                    <h6 class="mb-0 text-sm">{{count}}</h6>
+                  </div>
+                </div>
+              </td>
+              <td >
+                <div style="width: 300px;overflow-y: scroll;overflow-wrap: break-word;">
+                  <p class="text-xs font-weight-bold mb-0">{{mediaRet}}</p>
+                </div>
+              </td>
+              <td class="align-middle text-center text-sm">
+                <p class="text-xs font-weight-bold mb-0">{{varRet}}</p>
+              </td>
+              <td class="align-middle text-center">
+                <span class="text-secondary text-xs font-weight-bold">{{mediaFav}}</span>
+              </td>
+              <td class="align-middle text-center">
+                <span class="text-secondary text-xs font-weight-bold">{{varFav}}</span>
+              </td>
+            </tr>
+            
+            
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: "authors-table",
+  data() {
+    return {
+      count:0,
+      mediaRet:0,
+      varRet:0,
+      mediaFav:0,
+      varFav:0
+      }
+    },
+    async mounted() {
+      await axios.get('http://20.206.152.9:4444/tweets/countTweets').then((response) => {console.log(this.count=response.data)}),
+      await axios.get('http://20.206.152.9:4444/tweets/meanFav').then((response) => {console.log(this.mediaFav=response.data)}),
+      await axios.get('http://20.206.152.9:4444/tweets/varFav').then((response) => {console.log(this.varFav=response.data)}),
+      await axios.get('http://20.206.152.9:4444/tweets/meanRet').then((response) => {console.log(this.mediaRet=response.data)}),
+      await axios.get('http://20.206.152.9:4444/tweets/varRet').then((response) => {console.log(this.varRet=response.data)})
+    }
+};
+</script>
